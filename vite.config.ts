df0b6cb39971ed890e496df7bd2119cb3caeaad6
Your Name <you@example.com>
2026-02-13 10:39:43 -0500

    gaemoflifetiles

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,12 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: './', // IMPORTANT for GitHub Pages
+    base: './', 
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
     plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
